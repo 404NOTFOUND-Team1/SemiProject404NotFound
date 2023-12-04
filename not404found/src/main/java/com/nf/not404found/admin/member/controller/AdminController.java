87383c0a670821390d7569c802454b639fee8383
hyunkang-lee <klun102w@gmail.com>
2023-12-04 18:02:55 +0900

    test

--- not404found/src/main/java/com/nf/not404found/admin/member/controller/AdminController.java
+++ not404found/src/main/java/com/nf/not404found/admin/member/controller/AdminController.java
@@ -2,8 +2,12 @@ package com.nf.not404found.admin.member.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class AdminController {
+
+
 
 }
