dbe08bc863e8320b682a474453fc4275967f6b63
sooyeun-kim <sueyeon777@gmail.com>
2023-12-05 12:52:18 +0900

    12/5 12:52

--- not404found/src/main/java/com/nf/not404found/MainController.java
+++ not404found/src/main/java/com/nf/not404found/MainController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
-    @RequestMapping(value = {"/", "admin/product/modify"})
+    @RequestMapping(value = {"/", "/"})
     public String adminProductModify(){
 
-        return "admin/product/modify";
+        return "";
     }
 }
