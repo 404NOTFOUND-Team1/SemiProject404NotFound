f7932e33ca1e22634b68b3c6323c315f28b79b70
Seopia <wjdwltjq7289@gmail.com>
2023-12-05 12:55:08 +0900

    버그 수정

--- not404found/src/main/java/com/nf/not404found/admin/member/controller/AdminController.java
+++ not404found/src/main/java/com/nf/not404found/admin/member/controller/AdminController.java
@@ -2,8 +2,11 @@ package com.nf.not404found.admin.member.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class AdminController {
+
 
 }
