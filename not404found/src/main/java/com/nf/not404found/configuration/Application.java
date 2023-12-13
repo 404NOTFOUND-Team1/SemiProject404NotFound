7cfd7197ad8ab6058b230d74fb8ee669ceff2b72
hyunkang-lee <klun102w@gmail.com>
2023-12-13 17:35:07 +0900

    23/12/13 12:52 = 상품수정 완료!!

--- not404found/src/main/java/com/nf/not404found/configuration/Application.java
+++ not404found/src/main/java/com/nf/not404found/configuration/Application.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = "com.nf.not404found")
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
