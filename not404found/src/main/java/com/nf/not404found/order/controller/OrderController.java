e7d18c5dc4e68af00ab69111ad5bc991b0a26622
kyeongjinha <hajin89@gmail.com>
2023-12-20 20:13:56 +0900

    12-20 20:13

--- not404found/src/main/java/com/nf/not404found/order/controller/OrderController.java
+++ not404found/src/main/java/com/nf/not404found/order/controller/OrderController.java
@@ -1,21 +1,29 @@
 package com.nf.not404found.order.controller;
 
 
-import com.nf.not404found.order.model.service.OrderServiceImpl;
+import com.nf.not404found.common.functions.UserInformation;
+import com.nf.not404found.order.model.dto.OrderDTO;
+import com.nf.not404found.order.model.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @Slf4j
 @RequestMapping("/order/*")
 public class OrderController {
 
-    private final OrderServiceImpl orderService;
-
-    public OrderController(OrderServiceImpl orderService) {
-        this.orderService = orderService;
+    private final OrderService service;
+    private final UserInformation user;
+    public OrderController(OrderService service,UserInformation user) {
+        this.service = service;
+        this.user = user;
     }
 
     @GetMapping("orderComplete")
@@ -36,14 +44,36 @@ public class OrderController {
         return "/order/orderFailure";
     }
 
-    @GetMapping("orderMember")
-    public String orderMemberPage(){
-
-        log.info("=============================================> orderMember 매핑 확인");
-
-
+//    @PostMapping(value = "orderMember", produces = "application/json; charset=utf-8")
+    @PostMapping("orderMember")
+    public String orderMemberPage(@RequestParam String productName,
+                                @RequestParam int amount,
+                                @RequestParam int mileage,
+                                @RequestParam int deliveryCost,
+                                @RequestParam String price,
+                                Model model) {
+        List<String> coupon = user.getCoupon();
+        System.out.println("coupon = " + coupon);
+        OrderDTO orderInfor = service.getOrderInfor(user.getId());
+        model.addAttribute("productName",productName);
+        model.addAttribute("amount",amount);
+        model.addAttribute("mileage",mileage);
+        model.addAttribute("deliveryCost",deliveryCost);
+        model.addAttribute("price",price);
+        model.addAttribute("grade",orderInfor.getGrade());
+        model.addAttribute("rate",orderInfor.getMileageRate());
+        model.addAttribute("mileage",orderInfor.getMileage());
+        model.addAttribute("email",user.getEmail());
+        model.addAttribute("name",user.getName());
+        model.addAttribute("phone",user.getPhone());
+        model.addAttribute("addr",user.getAddr());
+        model.addAttribute("addrDetail",user.getAddrDetail());
         return "/order/orderMember";
     }
+//    @GetMapping("orderMember")
+//    public String orderMemberPage(){
+//        return "/order/orderMember";
+//    }
 
     @GetMapping("orderNonMember")
     public String orderNonMemberPage(){
