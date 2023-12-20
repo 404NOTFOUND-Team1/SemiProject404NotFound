1ad22a453a2ef9d9322cbf93de1e7a39ce092502
hyunkang-lee <klun102w@gmail.com>
2023-12-20 10:54:15 +0900

    23/12/19 21:43 = 결제 완료!

--- not404found/src/main/java/com/nf/not404found/main/MainController.java
+++ not404found/src/main/java/com/nf/not404found/main/MainController.java
@@ -1,24 +1,27 @@
 package com.nf.not404found.main;
 
 
+import com.nf.not404found.account.model.dto.AccountDTO;
+import com.nf.not404found.account.model.service.AccountService;
+import com.nf.not404found.admin.account.model.dto.AdminAccountDTO;
+import com.nf.not404found.admin.account.model.service.AdminAccountServiceImpl;
 import com.nf.not404found.common.functions.UserInformation;
 import com.nf.not404found.main.mainservice.MainService;
 import com.nf.not404found.main.model.dto.MainPageProductDTO;
 import com.nf.not404found.product.model.dto.ProductPageDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
+@Slf4j
 public class MainController {
     private final MainService service;
-
+    
     private final UserInformation user;
     public MainController(MainService service, UserInformation user){
         this.service = service;
@@ -52,15 +55,6 @@ public class MainController {
         System.out.println(product_name);
         List<ProductPageDTO> list = service.getProduct(product_name);
 
-        Map<String, String> mapInfo = new HashMap<>();
-
-
-
-        mapInfo.put("status", user.getStatus());
-
-
-
-        mv.addObject("status", mapInfo);
         mv.addObject("products",list);
         mv.setViewName("productpage/productPage");
         return mv;
